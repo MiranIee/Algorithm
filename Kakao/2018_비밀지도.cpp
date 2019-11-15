2348c764f1f9b15d47ab921d4b9bf14c3d8253cd
miranlee <rannan09@naver.com>
2019-11-14 19:54:58 -0500

    1114_1

--- Kakao/2018_비밀지도.cpp
+++ Kakao/2018_비밀지도.cpp
@@ -1,42 +1,32 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <bitset>
+#include <algorithm>
 
 using namespace std;
 
-string binaryToString(string s){
-    for(int i=0;i<s.length();i++){
-        if(s[i]=='1'){
-            s[i]='#';
-        }else s[i]=' ';
-    }
-
-    return s;
-}
 
 string tenToTwo(int n,int num){
     int binary, sum=0;
     string result="";
 
-    for(int i=1;num>0;i*=10){
+ while(num>0){
         binary=num%2;
-        sum+=binary*i;
         num/=2;
-    }   
-    result=to_string(sum);
 
-    cout<<result<<endl;
-    if(result.length()<n){
-        for(int i=0;i<n-result.length();i++){
-            result.insert(0,"0");
-        }
-         //append는 끝에
+        if(binary==1)
+        result+="#";
+        else result+=" ";
+
+    }   
+
+    while(result.length()<n){
+        result.append(" ");
     }
-    cout<<endl;
-    cout<<result<<endl;
-    result= binaryToString(result);
-    cout<<endl;
+   
+   reverse(result.begin(),result.end());
+
+
     return result;
 }
 
@@ -45,8 +35,7 @@ string tenToTwo(int n,int num){
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
     vector<int> map;
-    int c=22|14;
-    cout<<c<<endl;
+
     for(int i=0;i<n;i++){
         map.push_back(arr1[i]|arr2[i]);
         answer.push_back(tenToTwo(n,map[i]));
