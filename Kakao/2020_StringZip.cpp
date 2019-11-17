aa6f52ac44d130b3acdb1c0262f3434170fe98d1
miranlee <rannan09@naver.com>
2019-11-17 00:05:41 -0500

    1117_1

--- Kakao/2020_StringZip.cpp
+++ Kakao/2020_StringZip.cpp
@@ -1,35 +1,60 @@
-#include <string>
 #include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int solution(string s) {
+int solution(string s)
+{
     int answer = 0;
-    string result="";
-    string temp="";
-    string before="";
-    int count=1;
+    int min_length=1000;
+    string result = "";
+    string temp = "";
+    string before = "";
+    int count = 1;
 
-    for(int i=1;i<s.length();i++){
-        for(int j=0;j<s.length();j+=i){
-            temp=s.substr(j,i);
-
-            if(temp==before){//같은 경우
+    for (int i = 1; i <= s.length(); i++)
+    {
+        for (int j = 0; j < s.length()-i; j += i)
+        {   
+            before=s.substr(j,i);
+            if (before == s.substr(j + i, i))
+            {
                 count++;
-            }else{
-                result.append(to_string(count));
-                result.append(temp);
-                count=0;
             }
-            before=temp;
+            else
+            {
+                if(count!=1){
+                temp.append(to_string(count));
+                temp.append(before);
+                count = 1;
+                }
+                else{
+                    temp.append(before);
+                    count=1;
+                }
+            }
         }
+        if(count==1){
+            temp.append(before);
+        }else{
+            temp.append(to_string(count));
+            temp.append(before);
+        }
+        cout<<temp<<endl;
+
+        if(temp.length()<min_length){
+            min_length=result.length();
+            result=temp;
+        }
+        temp="";
     }
-    cout<<result<<endl;
+    cout << result << endl;
     return result.length();
 }
 
-int main(){
-    int answer=solution("aabbaccc");
-    cout<<answer<<endl;
+int main()
+{
+    int answer = solution("aabbaccc");
+    cout << answer << endl;
 }
