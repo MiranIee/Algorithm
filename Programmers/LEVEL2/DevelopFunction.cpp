f2680befac5a183103ef362320ceef35dea11184
miranlee <rannan09@naver.com>
2019-12-29 16:30:18 -0700

    191229_?

--- Programmers/LEVEL2/DevelopFunction.cpp
+++ Programmers/LEVEL2/DevelopFunction.cpp
@@ -1,48 +1,27 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds)
 {
     vector<int> answer;
-    vector<int> day(progresses.size());
-    int count = 0;
-    int max = 0;
-    int d_cnt = 1;
-    while (count != progresses.size())
+
+    int day;
+    int max_day = 0;
+    for (int i = 0; i < progresses.size(); ++i)
     {
-        count = 0;
-        for (int i = 0; i < progresses.size(); i++)
-        {
-            if (progresses[i] < 100)
-            {
-                progresses[i] += speeds[i];
-                day[i] = day[i] + 1;
-            }
-            else
-            {
-                count++;
-            }
-        }
+        day = (99 - progresses[i]) / speeds[i] + 1;
+
+        if (answer.empty() || max_day < day)
+            answer.push_back(1);
+        else
+            ++answer.back();
+
+        if (max_day < day)
+            max_day = day;
     }
 
-    max = day[0];
-    for (int i = 1; i < day.size(); i++)
-    {
-        if (max >= day[i])
-        {
-            d_cnt++;
-        }
-        else
-        {
-            answer.push_back(d_cnt);
-            d_cnt = 1;
-            max = day[i];
-        }
-    }
-    answer.push_back(d_cnt);
     return answer;
 }
 
