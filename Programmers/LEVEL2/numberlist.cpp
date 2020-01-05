469db259db5988b800cc2ee3de7150783520f4b6
miranlee <rannan09@naver.com>
2020-01-05 13:20:04 -0700

    190105_correct

--- Programmers/LEVEL2/numberlist.cpp
+++ Programmers/LEVEL2/numberlist.cpp
@@ -7,17 +7,29 @@ using namespace std;
 bool solution(vector<string> phone_book)
 {
     bool answer = true;
+    string shrt = "";
+    string lng = "";
     for (int i = 0; i < phone_book.size(); i++)
     {
-        for (int j = i; j < phone_book.size(); j++)
+        for (int j = i + 1; j < phone_book.size(); j++)
         {
             if (phone_book[i].length() <= phone_book[j].length())
             {
-                if (phone_book[i] == phone_book[j].substr(0, phone_book[i].length()))
-                {
-                    return false;
-                }
+                shrt = phone_book[i];
+                lng = phone_book[j];
             }
+            else
+            {
+                shrt = phone_book[j];
+                lng = phone_book[i];
+            }
+
+            if (shrt == lng.substr(0, shrt.length()))
+            {
+                return false;
+            }
+            shrt = "";
+            lng = "";
         }
     }
     return answer;
