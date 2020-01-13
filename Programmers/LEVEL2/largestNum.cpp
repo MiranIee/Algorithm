2e29d06314f46754623828f3575a6eafaa946898
miranlee <rannan09@naver.com>
2020-01-12 20:43:31 -0700

    largestNum

---
+++ Programmers/LEVEL2/largestNum.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string a, string b)
+{
+    if (b + a < a + b)
+        return true;
+    else
+        return false;
+}
+string solution(vector<int> numbers)
+{
+    string answer = "";
+    vector<string> temp;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        temp.push_back(to_string(numbers[i]));
+    }
+    sort(temp.begin(), temp.end(), compare);
+    for (int i = 0; i < temp.size(); i++)
+    {
+        answer += temp[i];
+    }
+    if (temp[0] == "0")
+        answer = "0";
+    return answer;
+}
+
+int main()
+{
+    string answer = solution({3, 30, 34, 5, 9});
+    cout << answer << endl;
+}
