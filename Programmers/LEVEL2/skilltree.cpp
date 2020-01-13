881cdd094e7061fb250436e8a309fbadd0b60094
miranlee <rannan09@naver.com>
2020-01-12 19:21:25 -0700

    skilltree_success

--- Programmers/LEVEL2/skilltree.cpp
+++ Programmers/LEVEL2/skilltree.cpp
@@ -7,12 +7,31 @@ using namespace std;
 int solution(string skill, vector<string> skill_trees)
 {
     int answer = 0;
-    string temp = "";
+    string tempStr = "";
+    int tempPos = 0;
+    bool chk = false;
+
     for (int i = 0; i < skill_trees.size(); i++)
     {
-        temp = skill_trees[i];
-        for (int j = 0; j < temp.length(); j++)
+        tempStr = skill_trees[i];
+        chk = false;
+
+        for (int j = 1; j < skill.length(); j++)
         {
+            tempPos = tempStr.find(skill[j]);
+            if (tempPos != -1) // 존재하는 경우
+            {
+                string temp = tempStr.substr(0, tempPos);
+                if (temp.find(skill[j - 1]) == -1) //순서가 잘못된 경우
+                {
+                    chk = true;
+                    break;
+                }
+            }
+        }
+        if (!chk)
+        {
+            answer++;
         }
     }
     return answer;
