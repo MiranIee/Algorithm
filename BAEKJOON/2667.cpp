f7bdae8bff3c9ab976ab771b45c2e58c96385219
miranlee <rannan09@naver.com>
2020-02-23 20:02:21 -0700

    2667

---
+++ BAEKJOON/2667.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX 25
+
+typedef struct
+{
+    int x, y;
+} Dir;
+
+Dir naver[4] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+int N;
+int house_cnt; //각 단지 내 집수
+string graph[MAX];
+vector<int> danzi;
+bool visited[MAX][MAX];
+
+void DFS(int x, int y)
+{
+    house_cnt++;
+    visited[x][y] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int nx = x + naver[i].x;
+        int ny = y + naver[i].y;
+
+        if (0 <= nx && nx < N && 0 <= ny && ny < N)
+            if (graph[nx][ny] == '1' && visited[nx][ny] == false)
+                DFS(nx, ny);
+    }
+}
+
+int main()
+{
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> graph[i]; //한줄씩 입력받음
+    }
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            if (graph[i][j] == '1' && visited[i][j] == false)
+            {
+                house_cnt = 0;
+                DFS(i, j);
+                danzi.push_back(house_cnt);
+            }
+        }
+
+    sort(danzi.begin(), danzi.end());
+    cout << danzi.size() << endl;
+
+    for (int i = 0; i < danzi.size(); i++)
+        cout << danzi[i] << endl;
+
+    return 0;
+}
