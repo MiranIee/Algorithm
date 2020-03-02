37626cc6ec42b88619c761c8d52663eaa5b376c7
miranlee <rannan09@naver.com>
2020-03-01 19:23:30 -0700

    Max 추출, 종료 조건 추가해야함

--- BAEKJOON/7576.cpp
+++ BAEKJOON/7576.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <queue>
+#include <algorithm>
 #define MAX 1000
 
 using namespace std;
-int map[MAX][MAX];
+pair<int, int> map[MAX][MAX];
 bool visited[MAX][MAX];
 int dirx[4] = {-1, 1, 0, 0};
 int diry[4] = {0, 0, -1, 1};
@@ -31,15 +32,23 @@ void BFS(int x, int y)
 
             if (0 <= nx && nx < N && 0 <= ny && ny < M)
             {
-                if (!map[nx][ny])
+                if (!map[nx][ny].first && !visited[nx][ny])
                 {
-                    map[nx][ny] = 1;
+                    map[nx][ny].first = 1;
+                    map[nx][ny].second = map[x][y].second + 1;
                     visited[nx][ny] = true;
                     q.push(make_pair(nx, ny));
                 }
+                else if (map[nx][ny].first == 1 && visited[nx][ny] == true)
+                {
+                    if ((map[nx][ny].second > map[x][y].second + 1) && map[x][y].first != -1)
+                    {
+                        map[nx][ny].second = map[x][y].second + 1;
+                        q.push(make_pair(nx, ny));
+                    }
+                }
             }
         }
-        day++;
     }
 }
 
@@ -53,11 +62,17 @@ int main()
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
         {
-            if (map[i][j] && !visited[i][j])
+            if (map[i][j].first && !visited[i][j])
             {
                 BFS(i, j);
             }
         }
 
-    cout << day << endl;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+            cout << map[i][j].second;
+        cout << endl;
+    }
+    //cout << day << endl;
 }
