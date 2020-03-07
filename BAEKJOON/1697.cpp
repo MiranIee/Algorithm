5f5587c9a7d4ed5573fce4fb418b59c16fa99244
miranlee <rannan09@naver.com>
2020-03-07 16:45:21 -0700

    1697_2

--- BAEKJOON/1697.cpp
+++ BAEKJOON/1697.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <queue>
+#define MAX 100001
 using namespace std;
 
 int K;
 queue<pair<int, int>> q;
-int min_ = 9999999999999999999999;
-
+int check[MAX];
+int min_ = 999999999;
+//10000, 0이랑 x범위..
 void BFS()
 {
     int x, y;
@@ -16,13 +18,22 @@ void BFS()
         y = q.front().second;
         //temp = q.front().second;
         q.pop();
+
         if (x != K)
         {
-            if (y + 1 < min_)
+            if (x - 1 >= 0 && !check[x - 1])
             {
-                temp += to_string(x);
+                check[x - 1] = true;
                 q.push(make_pair(x - 1, y + 1));
+            }
+            if (x + 1 < MAX && !check[x + 1])
+            {
+                check[x + 1] = true;
                 q.push(make_pair(x + 1, y + 1));
+            }
+            if (x * 2 < MAX && !check[2 * x])
+            {
+                check[x * 2] = true;
                 q.push(make_pair(x * 2, y + 1));
             }
         }
@@ -38,10 +49,15 @@ int main()
 {
     int N;
     scanf("%d %d", &N, &K);
-    q.push(make_pair(N - 1, 1));
-    q.push(make_pair(N + 1, 1));
-    q.push(make_pair(N * 2, 1));
-    BFS();
+    if (N == K)
+        cout << 0 << endl;
+    else if ((N + 1 == K) || (N - 1 == K) || (N * 2 == K))
+        cout << 1 << endl;
+    else
+    {
+        q.push(make_pair(N, 0));
+        BFS();
 
-    cout << min_ << endl;
+        cout << min_ << endl;
+    }
 }
