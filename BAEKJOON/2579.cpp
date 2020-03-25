dd53c0d083c5dbf699f298ebff0cb9dd19980fa7
miranlee <rannan09@naver.com>
2020-03-24 23:35:07 -0600

    2579_1

--- BAEKJOON/2579.cpp
+++ BAEKJOON/2579.cpp
@@ -8,10 +8,41 @@ int stair[300];
 
 int DP(int n)
 {
-    dp[0] = 0;
-    for (int i = 1; i <= n; i++)
+    int cnt = 0;
+    int tmp = 0;
+    int i = 0;
+    while (i + 2 <= n)
     {
+        if ((i + 1 == n) || (i + 2 == n))
+        {
+            if (cnt < 3)
+                tmp += max(stair[i + 1], stair[i + 2]);
+            else
+                tmp += stair[i + 2];
+            break;
+        }
+        else if (cnt < 3) //연속해서 올라간 게 아니라면
+        {
+            if (stair[i + 1] > stair[i + 2])
+            {
+                tmp += stair[i + 1];
+                i++;
+            }
+            else if (stair[i + 1] > stair[i + 2])
+            {
+                tmp += stair[i + 2];
+                i += 2;
+            }
+            cnt++;
+        }
+        else //연속해서 올라갔다면
+        {
+            tmp += stair[i + 2];
+            i += 2;
+            cnt = 0;
+        }
     }
+    return tmp;
 }
 
 int main()
