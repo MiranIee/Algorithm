93b14c14d688d4740ad7a6e99766b41958749c10
miranlee <rannan09@naver.com>
2020-04-01 22:24:28 -0600

    1149_1

--- BAEKJOON/1149.cpp
+++ BAEKJOON/1149.cpp
@@ -5,10 +5,23 @@
 using namespace std;
 
 int RGB[MAX][3]; //0 RED 1 GREEN 2 BLUE
-int dp[MAX];
+int dp[MAX][3];
 
 int DP(int n)
 {
+    dp[0][0] = RGB[0][0];
+    dp[0][1] = RGB[0][1];
+    dp[0][2] = RGB[0][2];
+
+    for (int i = 1; i < n; i++)
+    {
+        dp[i][0] = min(dp[i - 1][1] + RGB[i][0], dp[i - 1][2] + RGB[i][0]);
+        dp[i][1] = min(dp[i - 1][0] + RGB[i][1], dp[i - 1][2] + RGB[i][1]);
+        dp[i][2] = min(dp[i - 1][1] + RGB[i][2], dp[i - 1][0] + RGB[i][2]);
+    }
+    int temp = dp[n - 1][0] > dp[n - 1][1] ? (dp[n - 1][1] > dp[n - 1][3] ? dp[n - 1][3] : dp[n - 1][1]) : (dp[n - 1][2] > dp[n - 1][3] ? dp[n - 1][3] : dp[n - 1][1]);
+    cout << temp << endl;
+    return (dp[n - 1][0] > dp[n - 1][1] ? (dp[n - 1][1] > dp[n - 1][3] ? dp[n - 1][3] : dp[n - 1][1]) : (dp[n - 1][2] > dp[n - 1][3] ? dp[n - 1][3] : dp[n - 1][1]));
 }
 
 int main()
@@ -19,12 +32,7 @@ int main()
         for (int j = 0; j < 3; j++)
             scanf("%d", &RGB[i][j]);
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < 3; j++)
-            cout << RGB[i][j] << " ";
-        cout << endl;
-    }
+    cout << DP(n) << endl;
 
     return 0;
 }
