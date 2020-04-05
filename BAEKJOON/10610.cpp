66db5c91317ccc8526dcf5b29821deea9d9d0076
miranlee <rannan09@naver.com>
2020-04-04 22:22:19 -0600

    10610_1

--- BAEKJOON/10610.cpp
+++ BAEKJOON/10610.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <algorithm>
 #include <stdio.h>
 #include <string>
+using namespace std;
 
+int solution(string s)
+{
+    int n = 0;
+    if (s.find("0") == -1)
+        return -1;
+    else
+    {
+        sort(s.begin(), s.end(), greater<int>());
+        return stoi(s);
+    }
+}
 int main()
 {
-    int N;
-    scanf("%d", &N);
-    int cnt = 1;
-    while (1)
-    {
-    }
+    string input;
+    cin >> input;
+    cout << solution(input) << endl;
+    return 0;
 }
