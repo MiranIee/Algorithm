47c639135c93fbeb9a6c5bf7f721bb08ce1d69c8
miranlee <rannan09@naver.com>
2020-04-04 20:34:43 -0600

    11047_2

---
+++ BAEKJOON/11047.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#define MAX 10
+using namespace std;
+
+int coin[MAX];
+
+int greedy(int n, int k)
+{
+    int i = 0;
+    int answer = 0;
+    int cnt = 0;
+    sort(coin, coin + n, greater<int>());
+
+    while (k != 0)
+    {
+        if (k - coin[i] > 0)
+        {
+            for (cnt = 0; k - cnt * coin[i] > 0; cnt++)
+            {
+            }
+            k -= cnt * coin[i];
+            answer += cnt;
+        }
+        i++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int N, K;
+    scanf("%d %d", &N, &K);
+    for (int i = 0; i < N; i++)
+        scanf("%d", &coin[i]);
+
+    cout << greedy(N, K) << endl;
+
+    return 0;
+}
