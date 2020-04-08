6dc99c28a93385063157b1db69e5fe953e037e7c
miranlee <rannan09@naver.com>
2020-04-07 20:48:49 -0600

    1065_pass

--- BAEKJOON/1065.cpp
+++ BAEKJOON/1065.cpp
@@ -7,14 +7,15 @@ int solution(int n)
 {
     int sum = 0;
     int second;
-    if (n <= 100)
+    if (n < 100)
         return n;
     else
     {
-        for (int i = 100; i < n; i++)
+        for (int i = 100; i <= n; i++)
         {
-            second = i - (i / 100 * 100) / 10;
-            if (abs(i / 100 - second) == abs(second - i % 10))
+            second = (i % 100) / 10;
+
+            if ((i / 100 - second) == (second - i % 10))
             {
                 sum++;
             }
