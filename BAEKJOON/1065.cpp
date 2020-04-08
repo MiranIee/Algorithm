f219cbaaadfe6e209805ca7ae3175d7959a0d519
miranlee <rannan09@naver.com>
2020-04-07 20:15:04 -0600

    1065_1

--- BAEKJOON/1065.cpp
+++ BAEKJOON/1065.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 using namespace std;
 
 int solution(int n)
 {
     int sum = 0;
+    int second;
     if (n <= 100)
-        return 99;
+        return n;
     else
     {
         for (int i = 100; i < n; i++)
         {
-                }
+            second = i - (i / 100 * 100) / 10;
+            if (abs(i / 100 - second) == abs(second - i % 10))
+            {
+                sum++;
+            }
+        }
+
+        return 99 + sum;
     }
-    return sum;
 }
 int main()
 {
