00237fd8dcac6c575fc15cc2eefde4265c8f6cb8
miranlee <rannan09@naver.com>
2020-04-07 19:56:41 -0600

    Create 1065.cpp

---
+++ BAEKJOON/1065.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int solution(int n)
+{
+    int sum = 0;
+    if (n <= 100)
+        return 99;
+    else
+    {
+        for (int i = 100; i < n; i++)
+        {
+                }
+    }
+    return sum;
+}
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    cout << solution(N) << endl;
+}
