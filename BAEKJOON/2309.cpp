c15845bb462935381edf9d8127648f4b014b0f4c
miranlee <rannan09@naver.com>
2020-04-14 21:38:39 -0600

    2309_2

--- BAEKJOON/2309.cpp
+++ BAEKJOON/2309.cpp
@@ -6,33 +6,30 @@ using namespace std;
 void solution(int shorter[9])
 {
     int sum = 0;
-    int n1 = 0;
-    int n2 = 1;
-    int i = 0;
+    int i, j;
     //2개씩 빼면서 계산하면 됨
-    while (sum != 100)
+    sort(shorter, shorter + 9);
+
+    for (i = 0; i < 9 && sum != 100; i++)
     {
-        if (i != n1 || i != n2)
-        {
-            sum += shorter[i];
-        }
-        if (n2 == 9 && sum != 100)
+        for (j = i + 1; j < 9 && sum != 100; j++)
         {
             sum = 0;
-            n1++;
-            n2 = n1;
-        }
-        else
-        {
-            n2++;
+            for (int k = 0; k < 9; k++)
+            {
+                if (k != i && k != j)
+                {
+                    sum += shorter[k];
+                }
+            }
         }
     }
-
-    sort(shorter, shorter + 9);
-    for (int i = 0; i < 9; i++)
+    for (int k = 0; k < 9; k++)
     {
-        if (i != n1 || i != n2)
-            cout << shorter[i] << endl;
+        if (k != (i - 1) && k != (j - 1))
+        {
+            cout << shorter[k] << endl;
+        }
     }
 }
 
