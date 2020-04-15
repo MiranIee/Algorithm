0409d866fe401724dc7ce140a1f5ff047a7cf57c
miranlee <rannan09@naver.com>
2020-04-15 13:41:31 -0600

    2231_2

--- BAEKJOON/2231.cpp
+++ BAEKJOON/2231.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include <string>
 using namespace std;
 
 int solution(int N)
 {
     int i;
     int num = N;
+    string s_N = to_string(N);
+    string s_I;
     int sum = 0;
-    int cnt = 0;
-    vector<int> digit;
 
-    while (num > 0)
-    {
-        digit.push_back(num % 10);
-        num = num / 10;
-        cnt++;
-    }
+    // while (num > 0)
+    // {
+    //     digit.push_back(num % 10);
+    //     num = num / 10;
+    //     cnt++;
+    // }
+    cout << "length: " << s_N.length() << endl;
 
-    for (i = N - cnt * 9; i < N; i++)
+    for (i = N - (s_N.length()) * 9; i < N; i++)
     {
+        s_I = to_string(i);
         sum = i;
-        for (int j = 0; j < digit.size(); j++)
+        for (int j = 0; j < s_I.length(); j++)
         {
-            sum += digit[j];
+            sum += s_I[j];
         }
         if (sum == N)
             break;
