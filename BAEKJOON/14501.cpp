f794df9f1379a216a2c8ad5963304a6aa0d25ee8
miranlee <rannan09@naver.com>
2020-04-20 23:52:53 -0600

    14501_pass2

--- BAEKJOON/14501.cpp
+++ BAEKJOON/14501.cpp
@@ -3,22 +3,23 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+#define MAX 16
+int T[MAX] = {0};
+int P[MAX] = {0};
 
-int solution(int N, vector<pair<int, int>> v)
+int solution(int N)
 {
     int temp_sum = 0;
     int DP[N + 1] = {0};
     DP[0] = 0;
-    DP[v[0].first] = v[0].second;
+    DP[T[0]] = P[0];
 
     for (int i = 1; i < N; i++)
     {
-        if (N - i >= v[i].first)
+        if (N - i >= T[i]) //실현 가능한지
         {
-            // if (DP[i - 1] < DP[i]) //받은 게 더 크다면
-
             DP[i] = max(DP[i - 1], DP[i]);
-            DP[i + v[i].first] = max(DP[i + v[i].first], DP[i] + v[i].second);
+            DP[i + T[i]] = max(DP[i + T[i]], DP[i] + P[i]);
         }
     }
 
@@ -28,13 +29,11 @@ int solution(int N, vector<pair<int, int>> v)
 int main()
 {
     int N, t1, t2;
-    vector<pair<int, int>> v;
 
     scanf("%d", &N);
     for (int i = 0; i < N; i++)
     {
-        scanf("%d %d", &t1, &t2);
-        v.push_back(make_pair(t1, t2));
+        scanf("%d %d", &T[i], &P[i]);
     }
-    cout << solution(N, v) << endl;
+    cout << solution(N) << endl;
 }
