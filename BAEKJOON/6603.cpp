965a79d8df9319319eb323ef77590bb771f401c6
miranlee <rannan09@naver.com>
2020-05-13 15:10:37 -0600

    6603_1

--- BAEKJOON/6603.cpp
+++ BAEKJOON/6603.cpp
@@ -5,9 +5,29 @@
 using namespace std;
 
 int S[MAX];
+bool check[MAX];
 
-void solution(int n)
+void DFS(int k, int n, int pos)
 {
+    if (n == 6)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            if (check[i] == true)
+                cout << S[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    else if (pos >= k)
+    {
+        return;
+    }
+
+    check[pos] = true;
+    DFS(k, n + 1, pos + 1);
+    check[pos] = false;
+    DFS(k, n, pos + 1);
 }
 
 int main()
@@ -15,11 +35,11 @@ int main()
     int k;
     while (k != 0)
     {
-        memset(S, 0, sizeof(S));
+        memset(S, 0, sizeof(S)); //배열 초기화, cstring
         scanf("%d", &k);
-
         for (int i = 0; i < k; i++)
             scanf("%d", &S[i]);
-        solution(k);
+
+        DFS(k, 0, 0);
     }
 }
