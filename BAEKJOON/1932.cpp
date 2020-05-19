1f98fb78e326cef4685135155d1ab3195f39a891
miranlee <rannan09@naver.com>
2020-05-18 22:30:22 -0600

    1932_time-over

---
+++ BAEKJOON/1932.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define MAX 300
+int arr[MAX][MAX];
+int dp[MAX][MAX];
+
+int DP(int n)
+{
+    dp[0][0] = arr[0][0];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + arr[i + 1][j]);
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + arr[i + 1][j + 1]);
+        }
+    }
+
+    return *max_element(&dp[n - 1][0], &dp[n - 1][n]);
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+            scanf("%d", &arr[i][j]);
+    }
+
+    cout << DP(N) << endl;
+}
