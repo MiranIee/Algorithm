f9f3f629d71d02163a391dee6ce846f7be9b8b82
miranlee <48276682+mmirann@users.noreply.github.com>
2021-03-27 15:40:22 +0900

    Update 3-2.py

--- CodingTestBook/3/3-2.py
+++ CodingTestBook/3/3-2.py
@@ -1,2 +1,14 @@
-m,n,k = map(int, input().split())
-data=list(map(int,input().split()))
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+
+count = m / (k + 1)  # 반복되는 횟수
+result = count * ((first * k) + second)
+
+count = m % (k + 1)  # 나머지 큰 수 반복
+result += count * first
+
+print(int(result))
