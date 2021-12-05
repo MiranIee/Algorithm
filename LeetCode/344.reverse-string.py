336c19547f126f80d82a08013b13f06f7f01cb4d
miranlee <48276682+mmirann@users.noreply.github.com>
2021-12-05 20:09:47 +0900

    LeetCode 344.Reverse String Python

---
+++ LeetCode/344.reverse-string.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=344 lang=python
+#
+# [344] Reverse String
+#
+
+# @lc code=start
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+
+# String 뒤집기: [::-1]
+# List 뒤집기: reverse()
+# @lc code=end
